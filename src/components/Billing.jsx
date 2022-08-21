e52fa2fbe75ff3729ebb4de75e3a862290320e14
oruchanasar <57956356+Oruchan-Asar@users.noreply.github.com>
2022-08-21 17:06:26 +0300

    correction apple img alt string

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -24,7 +24,7 @@ const Billing = () => (
       </p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-        <img src={apple} alt="google_play" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
+        <img src={apple} alt="apple_store" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
         <img src={google} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
       </div>
     </div>
