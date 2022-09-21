909f8c4fb0d4f300a5e02c5edd9daf1afbd0fd34
Suryaputhran12 <rammadhav28@gmail.com>
2022-09-21 11:13:26 +0000

    Update README.md

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+    {/*<img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />*/}
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
